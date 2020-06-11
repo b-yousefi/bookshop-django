129f551a34bc36a5fe5a1be9cf93327ae5e56de7
b-yousefi <b.yousefi2911@gmail.com>
2020-06-12 03:01:36 +0430

    [refactor] Refined user and category
    **Add settings to allow cross origin access
    -User:
    --split firstName and lastName
    -UserController:
    --override the PATCH method, encrypt passwrod before saving
    -UserRepository:
    --add findUser function to fetch user's data with username
    -Category:
    --add subCategory field
    -CategoryController, CategoryRepo
    --fetchAll method returns all the categories with their subCategories

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
+
+from api.models import Author
+from api.serializers import AuthorSerializer
+
+# initialize the APIClient app
+client = Client()
+
+
+class GetAllAuthorsTest(TestCase):
+    """ Test module for GET all puppies API """
+
+    def setUp(self):
+        Author.objects.create(
+            fullName='Jane Austen', birthday=datetime.date.today())
+        Author.objects.create(
+            fullName='Author2', description="test author2")
+        Author.objects.create(
+            fullName='Author3', description="test author3")
+        Author.objects.create(
+            fullName='Author4', description="test author4")
+
+    def test_get_all_authors(self):
+        # get API response
+        response = client.get(reverse('get_all_authors'))
+        # get data from db
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
